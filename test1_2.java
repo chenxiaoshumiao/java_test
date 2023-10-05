c4ed751f6d3c2645589edaea0ec3a5bb6c8e3dfc
chenxiaoshumiao <774764749@qq.com>
2023-10-05 18:40:35 +0800

    Java中用试除法求素数

---
+++ test1_2.java
@@ -0,0 +1,41 @@
+//public class test1_2 {
+//    public static void main(String[] args){
+//        int count=0;
+//        int i=2;
+//        for(int num=2;num<1e5;num++){
+//           for(i=2;i<num;i++){
+//               if(num%i==0){
+//                   break;
+//               }
+//           }
+//           if(i==num){
+//               count++;
+//               System.out.print(num+"\t");
+//               if(count%10==0){
+//                   System.out.println("\t");
+//           }
+//            }
+//        }
+//        }
+//    }
+public class test1_2 {
+    public static void main(String[] args){
+        int count = 0;
+        for(int num = 2; num < 1e5; num++){
+            boolean isPrime = true;
+            for(int i = 2; i <= Math.sqrt(num); i++){ // 只需要判断到num的平方根即可
+                if(num % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                count++;
+                System.out.printf("%-8d", num); // 使用Printf控制输出宽度为8个字符
+                if(count % 10 == 0){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
